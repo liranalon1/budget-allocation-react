1a80aa0b860c28c9894f5321b59259e8dad4559a
unknown <liranalon84@gmail.com>
2023-12-23 16:11:54 +0200

    init second tab content

--- src/components/BudgetEdit/BudgetEdit.jsx
+++ src/components/BudgetEdit/BudgetEdit.jsx
@@ -1,9 +1,20 @@
 import './BudgetEdit.scss';
+import { useContext, useState } from 'react';
+import { channelContext } from '../../App';
+import ChannelMonthRow from '../Channel/ChannelMonthRow/ChannelMonthRow';
 
 const BudgetEdit = () => {
+    const { channels, setChannels } = useContext(channelContext);
+
     return (
-        <div className="budget-edit">
+        <div className="budget-edit flex">
             <p>This is budget edit component</p>
+
+            {channels.map((channel, i) => (
+                <div className="channel-months-wrap" key={channel.id}>
+                    <ChannelMonthRow data={channel} channelIndex={i} />
+                </div>
+            ))}
         </div>
     );
 };
